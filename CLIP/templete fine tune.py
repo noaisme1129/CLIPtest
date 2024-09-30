729d88de1376df28e475327b8dac2fc75f9f45b1
Xiaoyi Huang <165528601+noaisme1129@users.noreply.github.com>
2024-09-30 11:04:04 -0400

    Update templete fine tune.py

--- CLIP/templete fine tune.py
+++ CLIP/templete fine tune.py
@@ -6,22 +6,22 @@ from torchvision.datasets import CIFAR100
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
-# 确定使用的计算设备
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# 初始化 CLIP 模型
+
 model, _, preprocess_fn = open_clip.create_model_and_transforms('convnext_base_w', pretrained='laion2b_s13b_b82k')
 model.to(device)
 
-# 计算输出需要微调的参数数量
+
 num_params_to_tune = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print(f"number of fine tune parameter: {num_params_to_tune}")
 
-# 加载 CIFAR-100 数据集
+
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),  # 调整图像大小以适应 ConvNeXt 的输入
+    transforms.Resize((224, 224)),  
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # 标准化
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  
 ])
 
 train_data = CIFAR100(root="./data", download=True, train=True, transform=transform)
@@ -30,17 +30,17 @@ test_data = CIFAR100(root="./data", download=True, train=False, transform=transf
 train_loader = DataLoader(train_data, batch_size=16, shuffle=True)
 test_loader = DataLoader(test_data, batch_size=16, shuffle=False)
 
-# 可以使用不同的template
+# diffenet templete
 text_labels = [f"a photo of a {label}" for label in train_data.classes]
 # text_labels = [f"an image showing a {label}" for label in train_data.classes]
 # text_labels = [f"this is a {label}" for label in train_data.classes]
 text_inputs = open_clip.tokenize(text_labels).to(device)
 
-# 定义损失函数和优化器
+
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
-# 训练模型
+# training
 num_epochs = 5
 for epoch in range(num_epochs):
     model.train()
@@ -50,7 +50,7 @@ for epoch in range(num_epochs):
 
         optimizer.zero_grad()
 
-        # 编码图像与文本
+        #
         img_features = model.encode_image(batch_images)
         img_features = img_features / img_features.norm(dim=-1, keepdim=True)  # 避免就地操作
 
